36eea2c33edd17c498fcf6317ddf6e52eaabbe17
Nick Amorim <nicholas.amorim@shopify.com>
2024-08-04 23:27:43 -0400

    Add benchmark for large kv pair

--- benches/synchronizer.rs
+++ benches/synchronizer.rs
@@ -7,35 +7,58 @@ use prost::Message;
 
 /// Example data-structure shared between writer and reader(s)
 #[derive(Message, PartialEq)]
-pub struct HelloWorld {
+pub struct CacheEntry {
     #[prost(uint32, tag = "1")]
     pub version: u32,
-    #[prost(string, repeated, tag = "2")]
-    pub messages: Vec<String>,
+    #[prost(bytes = "bytes", tag = "2")]
+    pub key: ::prost::bytes::Bytes,
+    #[prost(bytes = "bytes", tag = "3")]
+    pub value: ::prost::bytes::Bytes,
 }
 
 pub fn bench_synchronizer(c: &mut Criterion) {
     let mut synchronizer = Synchronizer::new("/tmp/hello_world".as_ref());
-    let data = HelloWorld {
+    let normal_data = CacheEntry {
         version: 7,
-        messages: vec!["Hello".to_string(), "World".to_string(), "!".to_string()],
+        key: "key".as_bytes().into(),
+        value: "value".as_bytes().into(),
+    };
+    let big_data = CacheEntry {
+        version: 7,
+        key: "k".repeat(250).into(),
+        value: "v".repeat(250).into(),
     };
 
     let mut group = c.benchmark_group("synchronizer");
     group.throughput(Throughput::Elements(1));
 
-    group.bench_function("write", |b| {
+    group.bench_function("normal_data/write", |b| {
         b.iter(|| {
             synchronizer
-                .write(black_box(&data), Duration::from_nanos(10))
+                .write(black_box(&normal_data), Duration::from_nanos(10))
                 .expect("failed to write data");
         })
     });
 
-    group.bench_function("read", |b| {
+    group.bench_function("big_data/write", |b| {
         b.iter(|| {
-            let archived = unsafe { synchronizer.read::<HelloWorld>().unwrap() };
-            assert_eq!(archived.version, data.version);
+            synchronizer
+                .write(black_box(&big_data), Duration::from_nanos(10))
+                .expect("failed to write data");
+        })
+    });
+
+    group.bench_function("normal_data/read", |b| {
+        b.iter(|| {
+            let entry = unsafe { synchronizer.read::<CacheEntry>().unwrap() };
+            assert_eq!(entry.version, normal_data.version);
+        })
+    });
+
+    group.bench_function("big_data/read", |b| {
+        b.iter(|| {
+            let entry = unsafe { synchronizer.read::<CacheEntry>().unwrap() };
+            assert_eq!(entry.version, big_data.version);
         })
     });
 }
